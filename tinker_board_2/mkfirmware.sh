6f8ee5c18c7ac0136ca08be29fc345947f78cbe0
Rock Shen <rock_shen@asus.com>
2022-12-06 17:01:41 +0800

    mkfirmware.sh: update script for Tinker Board 2
    Change-Id: Id957951fe1f587e31623fa69238de7d4ea8f1184
    Signed-off-by: Rock Shen <rock_shen@asus.com>

--- tinker_board_2/mkfirmware.sh
+++ tinker_board_2/mkfirmware.sh
@@ -7,9 +7,9 @@ if ! which fakeroot &>/dev/null; then
     exit -1
 fi
 
-PARAMETER=device/rockchip/$RK_CHIP/$RK_PARAMETER
+PARAMETER=device/rockchip/tinker_board_2/$RK_PARAMETER
 MISC_IMG=device/rockchip/common/images/${RK_MISC:-blank-misc.img}
-MKIMAGE=device/rockchip/common/mk-image.sh
+MKIMAGE=device/rockchip/tinker_board_2/mk-image.sh
 
 message() {
     echo -e "\e[36m $@ \e[0m"
