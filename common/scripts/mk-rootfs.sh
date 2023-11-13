5b614223724deab0e3a3b6dcd7bd133eaa530ff3
Leslie Yu <leslie_yu@asus.com>
2023-11-13 15:07:36 +0800

    Check whether the directory rootfs-base exists
    Change-Id: Ib22d2e7aa79001590c2ad2ed6ed1285830c9626a

--- common/scripts/mk-rootfs.sh
+++ common/scripts/mk-rootfs.sh
@@ -124,7 +124,12 @@ build_debian()
 	message "=========================================="
 
 	cd debian
-	ROOTFS_BASE_DIR="../rootfs-base"
+
+  ROOTFS_BASE_DIR="../rootfs-base"
+  if [ ! -e $ROOTFS_BASE_DIR ]; then
+    ROOTFS_BASE_DIR="."
+  fi
+
 	if [ ! -f linaro-$RK_DEBIAN_VERSION-$ARCH.tar.gz ]; then
 		RELEASE=$RK_DEBIAN_VERSION TARGET=desktop ARCH=$ARCH \
 			./mk-base-debian.sh
